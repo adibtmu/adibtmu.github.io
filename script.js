324672883c565ce1932d959e5793b5327d283bb4
adibtmu <adib.chowdhury@torontomu.ca>
2024-02-24 12:42:16 -0500

    fixing button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function askForName() {
-    var name = prompt("Please enter your name:");
-    if (name != null && name != "") {
-        document.getElementById("output").innerHTML = "Hello, " + name + "!";
+    var name = prompt("Enter your name:");
+    if (name != null && name != "") { //checks that valid input was entered
+        document.getElementById("output").innerHTML = "Welcome " + name;
+    }
+    else { //prompts user to try again if invalid input entered
+        document.getElementById("output").innerHTML = "Invalid input. Please try again.";
     }
 }
